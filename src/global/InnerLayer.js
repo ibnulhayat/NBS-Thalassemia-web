44c9b67c784bf14eeb251fea74077148fa0f225a
Ibnul Hayat <53576655+ibnulhayat@users.noreply.github.com>
2023-11-13 19:20:26 +0600

    add drawer for mobile view

--- src/global/InnerLayer.js
+++ src/global/InnerLayer.js
@@ -2,11 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
 import * as Service from '../AllService'
 import { useLocation, useNavigate } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import LogoutIcon from '@mui/icons-material/Logout';
+
 
 export default function InnerLayer({children}) {
   const navigate = useNavigate()
-  const [selectTab, setSelectTab] = useState("")
   const location = useLocation();
+  const [selectTab, setSelectTab] = useState("")
+  const [drawerOpen, setDrawerOpen] = useState(false)
  
   console.log("location ",location.pathname)
 
@@ -19,12 +24,14 @@ export default function InnerLayer({children}) {
     navigate('/')
   }
 
+
+
   return (
       <div>
         <div id="logo">
           <span className="big-logo">NBS Thalassemia</span>
         </div>
-        <div id="left-menu">
+        <div className={`left-menu ${drawerOpen ? "drawerOpen" : ""}`}>
           <div className='m-2'>
             <div 
               className={`left-menu-item ${selectTab === '/dashboard'? 'active': ''}`}
@@ -33,7 +40,7 @@ export default function InnerLayer({children}) {
               <span >Dashboard</span>
               {/* <Link to={"/dashboard"} /> */}
             </div>
-            <div className={`left-menu-item ${selectTab === '/baby-info'? 'active': ''}`}
+            <div className={`left-menu-item ${selectTab.includes('baby-info')? 'active': ''}`}
               onClick={()=> navigate('/baby-info')}
             >
               <span>Baby Info</span>
@@ -56,35 +63,30 @@ export default function InnerLayer({children}) {
               <span>Edit SMS</span>
             </div>
 
-            <div className={`left-menu-item`}
+            {/* <div className={`left-menu-item`}
               onClick={()=> ClickLogout()}>
               <span>Logout</span>
-            </div>
+            </div> */}
           </div>
         </div>
         <div id="main-content">
           <div id="header">
-            {/* <div className="float-end logout">
+            <div className="float-start toggol-icon">
+              {
+                drawerOpen?
+                  <CloseIcon onClick={()=>setDrawerOpen(!drawerOpen)} fontSize="large" />
+                : <MenuIcon onClick={()=>setDrawerOpen(!drawerOpen)} fontSize="large" />
+              }
+              
+            </div>
+            <div className="float-end logout" onClick={()=>ClickLogout()}>
+              <LogoutIcon fontSize="large" />
               <span>Logout</span>
-            </div> */}
+            </div>
           </div>
           <div id="page-container">
             <div className="card">
               {children}
-              {/* {
-                selectTab === 'Dashboard'?
-                  <Dashboard />
-                : selectTab === 'Addbaby'?
-                  <AddBabyForm />
-                : selectTab === 'AddHospital'?
-                  <AddNewHospital />
-                : selectTab === 'AddNurs'?
-                  <AddNewNurs />
-                : selectTab === 'editsms'?
-                  <EditSMS />
-                :null 
-              } */}
-              
             </div>
           </div>
         </div>
