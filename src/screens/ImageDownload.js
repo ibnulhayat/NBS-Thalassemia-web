98610b06318224abe5879e237b00d8be14e40869
Ibnul Hayat <53576655+ibnulhayat@users.noreply.github.com>
2023-11-15 00:24:57 +0600

    add try catch

--- src/screens/ImageDownload.js
+++ src/screens/ImageDownload.js
@@ -1,25 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import { useEffect } from 'react'
-import { useNavigate, useSearchParams } from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 import * as Service from './../AllService'
 import loader from './../image/loading.gif'
 
 export default function ImageDownload(){
-    const navigate = useNavigate()
     const params = useSearchParams()
     const id = params[0].get('id')
     const image = params[0].get('im')    
     const [loading, setLoading] = useState(true)
     const [url, setUrl] = useState('')
 
-    console.log("params", id, image)
+    // console.log("params", id, image)
     useEffect(()=>{
         Call()
 
     },[])
 
     const Call = async() =>{
+       try {
         const response = await Service.DownloadPatientReport(id, image)
         // console.log("response", response)
         if(response?.code == 0){
@@ -28,12 +28,15 @@ export default function ImageDownload(){
             // navigate(-1)
             // window.open(response?.data?.urls?.[0],'_blank')
         }
+       } catch (error) {
+        console.log(error)
+       }
        
     }
     return (
         <div className='modal-main'>
             {   
-                loading?<img src={loading} alt="test" className='loader'/>
+                loading?<img src={loader} alt="test" className='loader'/>
                 : <img src={url} alt="test" />
             }
         </div>
